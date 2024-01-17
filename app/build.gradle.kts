51ade500856ece8d6f8fd537466c3cc29ab92f7b
Suyash Mittal <suyash8514@gmail.com>
2024-01-18 04:01:22 +0530

    reverted version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.suyash.creditmanager"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.01"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
